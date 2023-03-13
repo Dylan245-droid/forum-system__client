98c1d9ca3517f0cc5d0b93c31e4255581ec7bd73
Dylan245-droid <danielebang762@gmail.com>
2023-03-13 16:19:10 +0100

    13/03/2023

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Comments from "../utils/Comments";
+import Likes from "../utils/Likes";
 import Nav from "./Nav";
 
 const Home = () => {
   const [thread, setThread] = useState("");
+  const [threadList, setThreadList] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkUser = () => {
+      if (!localStorage.getItem("_id")) {
+        navigate("/");
+      } else {
+        fetch("http://localhost:4000/api/all/thread")
+          .then((res) => res.json())
+          .then((data) => {
+            setThreadList(data.threads)
+          })
+          .catch((err) => console.log(err));
+      }
+    };
+    checkUser();
+  }, [navigate]);
+
+  const createThread = (e) => {
+    fetch("http://localhost:4000/api/create/thread", {
+      method: "POST",
+      body: JSON.stringify({
+        thread,
+        userId: localStorage.getItem("_id"),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.message);
+        setThreadList(data.threads);
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ thread });
+    createThread();
     setThread("");
   };
   return (
@@ -15,18 +55,39 @@ const Home = () => {
       <main className="home">
         <h2 className="homeTitle">Create a Thread</h2>
         <form className="homeForm" onSubmit={handleSubmit}>
-          <div className="home__container">
-            <label htmlFor="thread">Title / Description</label>
-            <input
-              type="text"
-              name="thread"
-              required
+          <label htmlFor="reply">Title/Description</label>
+          <div className="formContent">
+            <textarea
+              rows={5}
               value={thread}
               onChange={(e) => setThread(e.target.value)}
+              type="text"
+              name="reply"
+              className="modalInput"
             />
+
+            <button className="modalBtn">SEND</button>
           </div>
-          <button className="homeBtn">CREATE THREAD</button>
         </form>
+
+        <div className="thread__container">
+          {threadList.map((thread) => (
+            <div className="thread__item" key={thread.id}>
+              <p>{thread.title}</p>
+              <div className="react__container">
+                <Likes
+                  numberOfLikes={thread.likes.length}
+                  threadId={thread.id}
+                />
+                <Comments
+                  numberOfComments={thread.replies.length}
+                  threadId={thread.id}
+                  title={thread.title}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
